9bab37696d1e4abe8b88362b026f9cd43f95ae7f
sheyenne carvalho <ennecarvalho22@gmail.com>
2025-11-16 03:14:01 -0400

    fix(dashboard): update Karine avatar URL to working v9 API

--- front-end/src/pages/Dashboard.jsx
+++ front-end/src/pages/Dashboard.jsx
@@ -29,7 +29,7 @@ export default function Dashboard() {
                   {/* Primeiro lugar */}
                   <div className="rank-item first">
                     <div className="rank-avatar" aria-hidden>
-                      <img src="https://api.dicebear.com/7.x/avataaars/svg?seed=Karine&skinColor=brown&hairColor=black&accessoriesType=Blank&clothesColor=blue&facialHairType=Blank&topType=LongHairBraids&eyeType=Happy&mouthType=Smile" alt="Avatar de Karine Andrade" />
+                      <img src="https://api.dicebear.com/9.x/avataaars/svg?seed=Karine&backgroundColor=b6e3f4&skinColor=brown&hair=longHairBraided&hairColor=black&top=longHairBraids&eyes=happy&mouth=smile" alt="Avatar de Karine Andrade" />
                     </div>
                     <div className="name-chip">Karine Andrade</div>
                     <div className="podium-card first">
